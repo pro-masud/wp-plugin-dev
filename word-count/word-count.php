def03f7f67425edeedc9732ab3f9c56d04602fdd
pro-masud <promasudbd@gmail.com>
2024-04-04 13:56:38 +0600

    Plugin Into Successfully working

---
+++ word-count/word-count.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Word Count
+ * Plugin URI: promasudbd.com
+ * Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, saepe!
+ * Version: 1.0.0
+ * Author: Masud Rana
+ * Author URI: promasudbd.com
+ * License: GPLv2 or later
+ * Text Domian: word-count
+ * Domain Path: /languages/
+*/
