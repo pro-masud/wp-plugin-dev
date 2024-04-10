758ac628c3cbc957191f115a4409ba495ab59c8a
pro-masud <promasudbd@gmail.com>
2024-04-10 16:08:20 +0600

    my first plugin creation complete my plugin name is: word count

--- word-count/word-count.php
+++ word-count/word-count.php
@@ -10,3 +10,24 @@
  * Text Domian: word-count
  * Domain Path: /languages/
 */
+
+
+/**
+ * plugin text domian loading function
+ * */ 
+function word_count_text_domain_load(){
+    load_plugin_textdomain('word-count');
+}
+
+ add_action("plugins_loaded", "word_count_text_domain_load");
+
+
+//  the content filter 
+function word_count_filter_action($content){
+    $words = strip_tags($content);
+    $word_count = str_word_count($words);
+    $label = __("Word Count Text Here:", "word-count");
+    $content .= sprintf("<h2>%s : %s</h2>", $label, $word_count);
+    return $content;
+}
+add_filter('the_content', 'word_count_filter_action');
